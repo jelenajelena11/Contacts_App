55c87b8413fe465a5e316e8923b00fbc8987eb5b
Jelena Stojanovic <stojanovicj97@gmail.com>
2022-07-29 17:47:54 +0200

    Added label interface

--- src/interfaces/Contact.ts
+++ src/interfaces/Contact.ts
@@ -1,11 +1,11 @@
 import { Label } from "./Label";
 
-export interface Contact{
-    id: number;
-    name: string;
-    email: string;
-    phone_number: string;
-    isFavorite: boolean;
-    profile_photo: string;
-    label: Label;
+export interface Contact {
+  id: number;
+  name: string;
+  email: string;
+  phone_number: string;
+  isFavorite: boolean;
+  profile_photo: string;
+  label: Label;
 }
--- src/interfaces/Label.ts
+++ src/interfaces/Label.ts
@@ -1,4 +1,4 @@
-export interface Label{
-    id: number;
-    name: string;
+export interface Label {
+  id: number;
+  name: string;
 }
